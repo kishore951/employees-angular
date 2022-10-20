6cc97525690f35cf1a954549be23a3236919f398
kishore951 <kishoresakthivel951@gmail.com>
2022-10-20 20:53:47 +0530

    three

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
 const appRoutes:Routes=[
-
+{
+  path:"",component:AddEmployeeComponent
+}
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddEmployeeComponent
   ],
   imports: [
     BrowserModule,
